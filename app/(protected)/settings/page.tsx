f0817b3faeb721a9d053110433c880ccbf94be87
rahilbshah <98266707+rahilbshah@users.noreply.github.com>
2024-04-09 21:52:11 +0530

    add client and server page

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,19 +1,20 @@
-import { auth, signOut } from '@/auth';
+'use client';
+
+import { logout } from '@/actions/logout';
+import { useCurrentUser } from '@/hooks/use-current-user';
+import { useSession, signOut } from 'next-auth/react';
 import React from 'react';
 
-const SettingsPage = async () => {
-  const session = await auth();
+const SettingsPage = () => {
+  const user = useCurrentUser();
+  const onClick = () => {
+    logout();
+  };
   return (
-    <div>
-      {JSON.stringify(session)}
-      <form
-        action={async () => {
-          'use server';
-          await signOut();
-        }}
-      >
-        <button type="submit">Sign Out</button>
-      </form>
+    <div className="bg-white p-10 rounded-xl">
+      <button onClick={onClick} type="submit">
+        Sign out
+      </button>
     </div>
   );
 };
